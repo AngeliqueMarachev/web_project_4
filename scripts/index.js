2b19986305bc9e31bf17c426ff813e99ee0b5177
AngeliqueMarachev <angeliquealive@gmail.com>
2022-04-04 14:19:01 +0300

    Commit for 1st submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,26 @@
-// Modal 
-const openModalButton = document.querySelector('.profile__edit-button'); 
-const modal = document.querySelector('.popup'); 
-const overlay = document.querySelector('.popup__content');
+// Modal
+const openModalButton = document.querySelector(".profile__edit-button");
+const modal = document.querySelector(".popup");
+const overlay = document.querySelector(".popup__content");
 
-openModalButton.addEventListener('click', function () {
-    modal.style.display = 'flex';
-    overlay.style.display = 'block';
-
+openModalButton.addEventListener("click", function () {
+  modal.style.display = "flex";
+  overlay.style.display = "block";
 });
 
 // Profile
-const profileName = document.querySelector('.profile__name');
-const profileOccupation = document.querySelector('.profile__occupation');
+const profileName = document.querySelector(".profile__name");
+const profileOccupation = document.querySelector(".profile__occupation");
 
 // Form
 const profileForm = document.forms.popup__form;
 const profileFormNameInput = profileForm.elements.name;
 const profileFormOccupationInput = profileForm.elements.occupation;
 
-profileForm.addEventListener('submit', function (event) {
-    profileName.textContent = profileFormNameInput.value;
-    profileOccupation.textContent = profileFormOccupationInput.value;
-    modal.style.display = 'none';
-    overlay.style.display = 'none';
-    event.preventDefault();
-
+profileForm.addEventListener("submit", function (event) {
+  profileName.textContent = profileFormNameInput.value;
+  profileOccupation.textContent = profileFormOccupationInput.value;
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  event.preventDefault();
 });
-

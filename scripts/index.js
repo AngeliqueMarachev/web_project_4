a1375b1838124e295c6c332c8a353de95061eaa7
AngeliqueMarachev <angeliquealive@gmail.com>
2022-04-21 17:30:59 +0300

    JS first commit for help from tutors

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,68 @@
-// Modal
+///////////////
+// TEMPLATE
+//////////////
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+const placesList = document.querySelector(".gallery__grid");
+
+
+initialCards.forEach(card => {
+  const cardTemplate = document.querySelector("#card-template").content.querySelector(".gallery__card");
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".gallery__item");
+  const cardTitle = cardElement.querySelector(".gallery__text");
+
+  cardImage.style.backgroundImage = `url(${card.link})`;
+  cardTitle.textContent = card.name;
+
+  placesList.append(cardElement);
+});
+
+
+///////////////////
+//PROFILE MODAL
+//////////////////
+
+// Profile modal
 const openModalButton = document.querySelector(".profile__edit-button");
 const modal = document.querySelector(".popup");
 const closeModalButton = document.querySelector(".popup__close-button");
 
-// Form 
+// Profile form 
 const inputName = document.querySelector(".popup__input_type_name");
 const inputOccupation = document.querySelector(".popup__input_type_occupation");
 const profileName = document.querySelector(".profile__name");
 const profileOccupation = document.querySelector(".profile__occupation");
 const form = document.querySelector(".popup__form");
 
-// Open Modal
+
+// Profile open & close modal
 openModalButton.addEventListener("click", function () {
   modal.classList.add("popup_open");
 
@@ -21,7 +73,12 @@ openModalButton.addEventListener("click", function () {
   inputOccupation.value = profileOccupationInput 
 });
 
-// Submit Form
+closeModalButton.addEventListener("click", function () {
+  modal.classList.remove("popup_open")
+});
+
+
+// Profile form submit
 form.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
@@ -34,10 +91,23 @@ form.addEventListener("submit", function (evt) {
   modal.classList.remove("popup_open")
 });
 
-// Close Modal
-closeModalButton.addEventListener("click", function () {
-  modal.classList.remove("popup_open")
-});
+
+//////////////
+// NEW PLACE MODAL
+//////////////
 
 
 
+
+// TEM
+
+
+
+
+
+
+
+
+
+
+ 
